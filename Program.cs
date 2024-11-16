69f052afcfdc7dd9918f392ceba355fd952f19ce
mLoDar <mlodarcoding@gmail.com>
2024-11-16 23:22:10 +0100

    Added entry point The entry point contains initial settings and little information about the application and what controls are available.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,47 @@
-﻿namespace SnakeGame
+﻿using System.Text;
+
+
+
+
+
+namespace SnakeGame
 {
     internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello, World!");
+        LabelMethodBeginning:
+
+            Console.Title = "SnakeGame";
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.CursorVisible = false;
+
+            Console.Clear();
+            Console.SetCursorPosition(0, 4);
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+
+
+
+            Console.WriteLine("             ┏┓    ┓    ┏┓         ");
+            Console.WriteLine("             ┗┓┏┓┏┓┃┏┏┓ ┃┓┏┓┏┳┓┏┓  ");
+            Console.Write("             ┗┛┛┗┗┻┛┗┗  ┗┛┗┻┛┗┗┗   ");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("│");
+            Console.WriteLine("          ┌────────────────────────┘");
+            Console.WriteLine("          │ Movement -> Arrow Keys");
+            Console.WriteLine("          │ Exit at any time with ESC");
+            Console.WriteLine("                                     ");
+            Console.WriteLine("            Press any key to start   ");
+
+
+
+            if (Console.ReadKey().Key.Equals(ConsoleKey.Escape))
+            {
+                Environment.Exit(0);
+            }
+
+            goto LabelMethodBeginning;
         }
     }
 }
