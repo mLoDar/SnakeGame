e31942a080d2082d6717329f82a00418174ee27d
mLoDar <mlodarcoding@gmail.com>
2024-11-17 12:45:38 +0100

    Changed settings file Switched from 'const' to 'readonly' as the application can not read the variables. Will look into this later on, but for now it should work.

--- ApplicationSettings.cs
+++ ApplicationSettings.cs
@@ -4,21 +4,21 @@
     {
         internal class Playfield
         {
-            internal const int Height = 25;
-            internal const int Width = 25;
+            internal readonly int height = 25;
+            internal readonly int width = 25;
         }
 
         internal class Game
         {
-            internal const int SpeedInMilliseconds = 100;
+            internal readonly int speedInMilliseconds = 100;
         }
 
         internal class Symbols
         {
-            internal const char Empty = ' ';
-            internal const char SnakeHead = '+';
-            internal const char SnakeBody = '■';
-            internal const char Fruit = '#';
+            internal readonly char empty = ' ';
+            internal readonly char snakeHead = '+';
+            internal readonly char snakeBody = '■';
+            internal readonly char fruit = '#';
         }
     }
 }
