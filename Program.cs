834c8e45ea149b4bde2a22ce40dc0d4d0c919584
mLoDar <mlodarcoding@gmail.com>
2024-11-20 09:48:50 +0100

    Basic game structure added The base structure contains two parallel tasks, one of which is used to update/display the playing field. The second is used to read user input and change the current direction accordingly. The game logic is not yet implemented.

--- Program.cs
+++ Program.cs
@@ -20,9 +20,16 @@ namespace SnakeGame
     {
         private static SnakeDirection _currentDirection = SnakeDirection.Right;
 
+        private static readonly ApplicationSettings.Game _gameSettings = new();
+        private static readonly ApplicationSettings.Playfield _playfieldSettings = new();
+
+        private static Gamefield _gameField;
+
+        private static bool _gameEnded = false;
 
 
-        static void Main()
+
+        static async Task Main()
         {
         LabelMethodBeginning:
 
@@ -56,7 +63,77 @@ namespace SnakeGame
                 Environment.Exit(0);
             }
 
+
+
+            Console.Clear();
+            Console.SetCursorPosition(0, 4);
+
+            _gameEnded = false;
+            _currentDirection = SnakeDirection.Right;
+
+            _gameField = new Gamefield(_playfieldSettings.height, _playfieldSettings.width);
+            _gameField.Init();
+
+            _gameField.Display();
+
+
+
+            CancellationTokenSource cancellationTokenSource = new CancellationTokenSource();
+            CancellationToken cancellationToken = cancellationTokenSource.Token;
+
+            Task mainGame = MainGameLoop(cancellationToken);
+            Task readingTask = ReadDirection(cancellationToken);
+
+            await Task.WhenAny(mainGame, readingTask);
+
+            cancellationTokenSource.Cancel();
+
+
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("          GAME OVER");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("          Press any key to continue ...");
+
+
+
+            if (Console.ReadKey().Key.Equals(ConsoleKey.Escape))
+            {
+                Environment.Exit(0);
+            }
+
             goto LabelMethodBeginning;
+        }
+
+        private static async Task MainGameLoop(CancellationToken cancellationToken)
+        {
+            while (_gameEnded == false)
+            {
+                // Todo: Move snake
+
+                // Todo: Display updated game field
+
+                await Task.Delay(_gameSettings.speedInMilliseconds, cancellationToken);
+            }
+        }
+
+        private static async Task ReadDirection(CancellationToken cancellationToken)
+        {
+            while (_gameEnded == false)
+            {
+                if (cancellationToken.IsCancellationRequested)
+                {
+                    return;
+                }
+
+                if (Console.KeyAvailable == false)
+                {
+                    await Task.Delay(50, cancellationToken);
+                    continue;
+                }
+
+                // Todo: Read key and change direction
+            }
         }
     }
 }
